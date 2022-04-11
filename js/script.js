a1656f6c159c9976581814690323771c8c28b708
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-04-11 15:25:12 +0200

    add conditions js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("ciao")
+// Stampare nella console numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+console.log(i);
+
+    // Per i multipli di 3 stampare fizz
+    if (i % 3 === 0) {
+        fizzBuzzDom = "Fizz";
+    // Per i multipli di 5 stampare buzz
+    } else if (i % 5 === 0) {
+        fizzBuzzDom = "Buzz";
+    // Per i multipli di entrambi stampare fizzbuzz
+    
