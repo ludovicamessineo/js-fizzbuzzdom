be1114c04c4c1c6fc2fd7ff2df541044042f9b44
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-04-12 16:04:26 +0200

    add correction and style

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,18 +1,22 @@
+const rowContainer = document.querySelector(".row");
+console.log(rowContainer);
 // Stampare nella console numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    let result;
 
     // Per i multipli di 15 stampare fizzbuzz
     if (i % 15 === 0) {
-    console.log("FizzBuzz");
+        result = "FizzBuzz";
     // Per i multipli di 3 stampare fizz
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+        result ="Fizz";
     // Per i multipli di 5 stampare buzz
     } else if (i % 5 === 0) {
-       console.log("Buzz");
+        result ="Buzz";
     // Altrimenti visualizzo numero
     } else {
-        console.log(i);
+        result = i;
     }
 
+    rowContainer.innerHTML += `<div class="box ${result}">${result}</div>`;
 }
